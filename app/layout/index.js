f6519f9b34f342da65b782852883eac8ea22b92f
morshidulrahman <boydanger416@gmail.com>
2022-11-23 01:47:19 +0600

    cart add localstorage

--- app/layout/index.js
+++ app/layout/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer"
 import Header from './Header'
 import { auth } from '../configs/firebase'
 import { updateUser } from '../redux/slices/authSlice'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { removeUesr } from '../redux/slices/authSlice'
+import { selectItems, updateBasket } from '../redux/slices/basketSlice'
 function Layout({ children }) {
     const dispatch = useDispatch()
+    const cartItems = useSelector(selectItems)
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(async (user) => {
             if (user) {
@@ -19,8 +21,26 @@ function Layout({ children }) {
                 dispatch(removeUesr())
             }
         })
+
+        // get cartitems from the loacalstroge
+
+        const local_items = localStorage.getItem("CART_ITEMS") ? JSON.parse(localStorage.getItem("CART_ITEMS")) : []
+
+        dispatch(updateBasket(local_items))
+
         return unsubscribe
     }, [])
+
+    useEffect(() => {
+        if (!cartItems.length) return
+
+        // save the cart items to localstorage
+
+        const items = JSON.stringify(cartItems)
+        localStorage.setItem("CART_ITEMS", items)
+
+    }, [cartItems])
+
     return (
         <>
             <Header />
