52f23a8dc12e25c5d9dddb4967fff45586d2b798
morshidulrahman <boydanger416@gmail.com>
2023-01-10 00:18:52 +0600

    eslint

--- app/components/Product/ProductDetails.js
+++ app/components/Product/ProductDetails.js
@@ -1,6 +1,6 @@
-import Breadcrumb from "../shared/Breadcrumb"
-import FooterCard from "../shared/footercard"
-import Quantity from "../shared/Quantity"
+import Breadcrumb from "../shared/Breadcrumb";
+import FooterCard from "../shared/footercard";
+import Quantity from "../shared/Quantity";
 
 const ProductDetails = ({ price = 30.25, oldPrice }) => {
     return (
@@ -9,42 +9,74 @@ const ProductDetails = ({ price = 30.25, oldPrice }) => {
                 <div className="p-10 bg-white rounded-2xl">
                     <div className="flex flex-wrap lg:flex-row flex-col lg:gap-0 gap-7">
                         <div className="lg:w-2/5 w-full lg:border-r">
-                            <img src="https://i0.wp.com/demo4.drfuri.com/farmart2/wp-content/uploads/sites/11/2020/02/02_3a.jpg?fit=640%2C640&ssl=1" alt="" loading="lazy" />
+                            <img
+                                src="https://i0.wp.com/demo4.drfuri.com/farmart2/wp-content/uploads/sites/11/2020/02/02_3a.jpg?fit=640%2C640&ssl=1"
+                                alt=""
+                                loading="lazy"
+                            />
                         </div>
                         <div className="lg:w-[37%] w-full lg:px-10 pl-0">
                             <div className="text-color mb-5">
                                 <Breadcrumb />
                             </div>
-                            <h4 className="md:text-xl text-base font-bold mb-2 text-orange">MartFury</h4>
-                            <h1 className="text-title md:text-3xl text-xl font-semibold mb-5">Bar S - Classic Bun Length Franks</h1>
-                            <span className="text-color text-xs">⭐⭐⭐⭐⭐ (1 customer review)</span>
+                            <h4 className="md:text-xl text-base font-bold mb-2 text-orange">
+                                MartFury
+                            </h4>
+                            <h1 className="text-title md:text-3xl text-xl font-semibold mb-5">
+                                Bar S - Classic Bun Length Franks
+                            </h1>
+                            <span className="text-color text-xs">
+                                ⭐⭐⭐⭐⭐ (1 customer review)
+                            </span>
                             <div className="my-3">
                                 <span className="text-sm text-color mb-2 block">500gl</span>
                                 <div className="flex items-center gap-1">
-                                    <span className={`text-xl font-bold ${oldPrice ? 'text-orange' : 'text-green'}`}>${price}</span>
+                                    <span
+                                        className={`text-xl font-bold ${oldPrice ? "text-orange" : "text-green"
+                                            }`}
+                                    >
+                                        ${price}
+                                    </span>
                                     {oldPrice && (
-                                        <span className="text-lg line-through text-gray-400">${oldPrice}</span>
+                                        <span className="text-lg line-through text-gray-400">
+                                            ${oldPrice}
+                                        </span>
                                     )}
                                 </div>
                             </div>
                             <div className="my-5 bg-[#EBFAE9] w-fit px-3 py-2 border border-[#82d45e] rounded-md">
-                                Availability: <span className="text-green ml-1 font-bold">In stock</span>
+                                Availability:{" "}
+                                <span className="text-green ml-1 font-bold">In stock</span>
                             </div>
                             <div className="border-t"></div>
                             <div className="my-5">
                                 <span className="text-sm pb-2 block text-color">Quantity:</span>
                                 <div className="flex gap-3">
                                     <Quantity />
-                                    <button className="btn-white bg-primary px-5 py-2 rounded">Add to cart</button>
+                                    <button className="btn-white bg-primary px-5 py-2 rounded">
+                                        Add to cart
+                                    </button>
                                 </div>
                             </div>
                             <div className="border-t my-7"></div>
                             <div className="flex flex-col gap-2">
-                                <p className="text-gray-900"><span className="text-color">SKU:</span> #veg20967</p>
-                                <p className="text-gray-900"><span className="text-color">Brand:</span> mangogn</p>
-                                <p className="text-gray-900"><span className="text-color">Vendor:</span> MartFury</p>
-                                <p className="text-gray-900"><span className="text-color">Categories:</span> Breakfast Cereals, Chocolate & Sweets, Crisps, Snacks & Nuts, Food Cupboard, Tins & Cans</p>
-                                <p className="text-gray-900"><span className="text-color">Tag:</span> Best Seller</p>
+                                <p className="text-gray-900">
+                                    <span className="text-color">SKU:</span> #veg20967
+                                </p>
+                                <p className="text-gray-900">
+                                    <span className="text-color">Brand:</span> mangogn
+                                </p>
+                                <p className="text-gray-900">
+                                    <span className="text-color">Vendor:</span> MartFury
+                                </p>
+                                <p className="text-gray-900">
+                                    <span className="text-color">Categories:</span> Breakfast
+                                    Cereals, Chocolate & Sweets, Crisps, Snacks & Nuts, Food
+                                    Cupboard, Tins & Cans
+                                </p>
+                                <p className="text-gray-900">
+                                    <span className="text-color">Tag:</span> Best Seller
+                                </p>
                             </div>
                         </div>
                         <div className="lg:w-[23%] w-full">
@@ -70,14 +102,16 @@ const ProductDetails = ({ price = 30.25, oldPrice }) => {
                             <div className="bg-[#F3F3F3] px-5 mt-5 py-7 rounded-md">
                                 <p className="font-bold text-title">Hotline Order:</p>
                                 <span className="text-color">Mon - Fri: 7:00 am - 18:00PM</span>
-                                <p className="text-xl md:text-2xl font-bold text-title mt-5">+856 332 - 466</p>
+                                <p className="text-xl md:text-2xl font-bold text-title mt-5">
+                                    +856 332 - 466
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProductDetails
+export default ProductDetails;
