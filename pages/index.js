5c9df5652b94e531fccbbe50cbe3f296f25ef5b8
morshidulrahman <boydanger416@gmail.com>
2023-01-11 14:46:21 +0600

    index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import Homecategory from '../app/components/Home/Homecategory'
-import Homehero from '../app/components/Home/Homehero'
-import FeatureBrands from '../app/components/Home/FeatureBrands'
-import TodaysDeal from '../app/components/Home/TodaysDeal'
-import Homeposter from '../app/components/Home/Homeposter'
-import EssentialProduct from '../app/components/Home/Essentialproduct'
-import Healthydaly from '../app/components/Home/Healthydaly'
-import { setBackgroundImage, getPage, getPageServer } from '../app/utils/helper'
-import Admin from '../app/admin'
+import React from "react";
+import {
+  Essentialproduct,
+  FeatureBrands,
+  Homecategory,
+  Homehero,
+  Homeposter,
+  TodaysDeal,
+} from "../app/components/Home";
+import {
+  setBackgroundImage,
+  getPage,
+  getPageServer,
+} from "../app/utils/helper";
+import Admin from "../app/admin";
 function index({ page }) {
-  if (page === 'admin') return <Admin />
+  if (page === "admin") return <Admin />;
   return (
     <>
       <Homehero />
@@ -17,27 +22,37 @@ function index({ page }) {
       <FeatureBrands />
       <TodaysDeal />
       <Homeposter />
-      <div className="py-5" style={setBackgroundImage('https://demo4.drfuri.com/farmart2/wp-content/uploads/sites/11/2021/05/homepage-new-content-bg-scaled.jpg', { backgroundPosition: 'bottom center', backgroundColor: '#f3f3f3', backgroundSize: 'cover' })}>
-        <EssentialProduct />
+      <div
+        className="py-5"
+        style={setBackgroundImage(
+          "https://demo4.drfuri.com/farmart2/wp-content/uploads/sites/11/2021/05/homepage-new-content-bg-scaled.jpg",
+          {
+            backgroundPosition: "bottom center",
+            backgroundColor: "#f3f3f3",
+            backgroundSize: "cover",
+          }
+        )}
+      >
+        <Essentialproduct />
         <Healthydaly />
       </div>
     </>
-  )
+  );
 }
 
-export default index
+export default index;
 
 export async function getServerSideProps(context) {
-  let page
+  let page;
   const { req } = context;
   if (req) {
-    let host = req.headers.host
-    page = getPageServer(host) || null
+    let host = req.headers.host;
+    page = getPageServer(host) || null;
   }
 
   return {
     props: {
-      page: page
+      page: page,
     },
-  }
+  };
 }
